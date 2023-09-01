7726586f8676631f4bcaa3fca7c935355857052d
Rizky Ajie Kurniawan - Mac <rizkyak994@gmail.com>
2023-09-01 09:35:44 +0700

    init vue latest

--- konsultrik/src/main.js
+++ konsultrik/src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+const app = createApp(App)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
